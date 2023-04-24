5ec97af5f15960f385bdba245060f0cc357af2bd
Alison Sav <alisa.zarytska@troops-ext.fr>
2023-04-24 15:05:54 +0200

    decorator with arguments

--- simple_decorator.py
+++ simple_decorator.py
@@ -1,18 +1,19 @@
-def decorator_with_arguments(dec_arg_1, dec_arg_2):
+def decorator_with_arguments(mom, dad):
     """There is a decorator with arguments"""
+
     def decorator(func):
-        def function(func_arg_1, func_arg_2):
-            print(f'There are decorators arguments and functions arguments', dec_arg_1, dec_arg_2, func_arg_1, func_arg_2)
-            return func(func_arg_1, func_arg_2)
+        def function(son, daughter):
+            print('There are decorators arguments and functions arguments. All family: ', mom, dad, son, daughter)
+            return func(son, daughter)
 
         return function
 
     return decorator
 
 
-@decorator_with_arguments('Name_1', 'Name_2')
+@decorator_with_arguments('Mom', 'Dad')
 def new_func(arg1, arg2):
-    print(f'There are only functions arguments {arg1}, {arg2}')
+    print(f'There are children: {arg1}, {arg2}')
 
 
-new_func('Arg_1', 'Arg_2')
+new_func('Son', 'Daughter')
