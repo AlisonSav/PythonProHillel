a31ad4a3b1d1a74e37f585fd48fa0e7f32321c51
Alison Sav <alisa.zarytska@troops-ext.fr>
2023-04-24 15:00:06 +0200

    decorator with arguments

--- simple_decorator.py
+++ simple_decorator.py
@@ -1,21 +1,18 @@
-dec_list = []
+def decorator_with_arguments(dec_arg_1, dec_arg_2):
+    """There is a decorator with arguments"""
+    def decorator(func):
+        def function(func_arg_1, func_arg_2):
+            print(f'There are decorators arguments and functions arguments', dec_arg_1, dec_arg_2, func_arg_1, func_arg_2)
+            return func(func_arg_1, func_arg_2)
+
+        return function
+
+    return decorator
 
 
-def add_func(func):
-    """Add function's name to the list"""
-    dec_list.append(func.__name__)
-    return func
+@decorator_with_arguments('Name_1', 'Name_2')
+def new_func(arg1, arg2):
+    print(f'There are only functions arguments {arg1}, {arg2}')
 
 
-@add_func
-def summ(x, y):
-    return x + y
-
-
-@add_func
-def mul(x, y):
-    return x * y
-
-
-print(dec_list)
-
+new_func('Arg_1', 'Arg_2')
