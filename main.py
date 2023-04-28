0d56236696028ff73c5f63a136addd2d92368dbf
Alison Sav <alisa.zarytska@troops-ext.fr>
2023-04-28 14:39:04 +0200

    hw_2

--- main.py
+++ main.py
@@ -1,32 +1,60 @@
-from flask import Flask
+from flask import Flask, request
 from random import randint
 
 app = Flask(__name__)
 
-lst = []
 
+class User:
 
-@app.route("/")
-def show_items():
-    return f"<h3>Values in list are: {lst}<h3>"
+    def __init__(self, name='', language=''):
+        self.grade = ''
+        self.course = ''
+        self.name = name
+        self.language = language
 
+    @app.route("/login")
+    def login(self):
+        languages = {'Python', 'Java', 'JS', 'Ruby'}
 
-@app.route("/add_item")
-def add_item():
-    if len(lst) >= 10:
-        return f"<h3>Sorry! You can't add value if list's length more than 10!<h3>"
-    else:
-        lst.append(randint(1, 100))
-        return f"<h3>Updated list: {lst}<h3>"
+        name = input()
+        language = input()
+        if name.isalpha() and language in languages:
+            return f"""
+            <h3>Hello!<h3>
+            <form action="/add_course" method="POST">
+              <div>
+                <label for="course">Please enter course</label>
+                <input name ="course" id="add_course" value="str" />
+              </div>
+                <button>Add course</button>
+            </form> """
+        else:
+            if language not in languages:
+                return f'<h3>You chose incorrect language. Try again!<h3>'
+            else:
+                return f"<h3>Name can't contain digits or symbols<h3>"
 
+    @app.route("/add_course", methods=["POST"])
+    def get_course(self):
+        courses = {'Basic', 'Pro', 'Advanced'}
+        course = input()
+        if course not in courses:
+            return f"<h3>Sorry! You can select course only from list<h3>"
+        else:
+            self.course = course
+            return f"""<h3>Course for {self.name} is {self.course}<h3>
+            <a href="/">Return to Home page</a>
+            """
 
-@app.route("/delete_item")
-def delete_item():
-    if lst:
-        return f"<h3>Deleted item is: {lst.pop()}<h3>"
-    else:
-        return f"<h3>Your list {lst} is empty. You can't delete value<h3>"
+    @app.route("/add_grade", methods=["POST"])
+    def get_grade(self):
+        grade = randint(1, 13)
+        self.grade = grade
+        return f"""<h3>Grade for {self.name} is {self.grade}<h3>
+                <a href="/">Return to Home page</a>
+        """
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
+    user1 = User("Alisa", "Python")
